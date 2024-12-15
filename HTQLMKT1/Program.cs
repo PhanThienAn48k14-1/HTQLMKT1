f10b58e316351511f452f3bd16384eafee956794
PhanThienAn48k14-1 <phanthienan19082004@gmail.com>
2024-12-15 09:27:45 +0700

    QLHD2

--- HTQLMKT1/Program.cs
+++ HTQLMKT1/Program.cs
@@ -16,7 +16,7 @@ namespace HTQLMKT1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new KhachHang());
+            Application.Run(new NhanVien());
         }
     }
 }
